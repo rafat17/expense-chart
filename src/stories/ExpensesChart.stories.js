b60175be17309cd34f2cd7ffe3a53d25838df650
rafat17 <43632236+rafat17@users.noreply.github.com>
2023-11-22 22:22:31 +0600

    build: stories for ExpenseChart

---
+++ src/stories/ExpensesChart.stories.js
@@ -0,0 +1,25 @@
+import ExpenseChart from "./components/ExpenseChart";
+import dummyExpenseData from "./components/ExpenseChart/data/dummyExpenseData";
+
+export default {
+  title: "Example/ExpenseChart",
+  component: ExpenseChart,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const Primary = {
+  args: {
+    data: dummyExpenseData,
+    width: "561px",
+    height: "545px",
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: "ExpensesTab",
+  },
+};
